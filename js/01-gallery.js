dc97c02187f2570cba72282e0d7dfce883e09f73
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-17 02:26:42 +0300

    підключає basiclightbox та додає рендер зображень в галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItemsMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+    `).join('')
+};
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup(galleryItems));
+
+console.log(galleryItemsMarkup(galleryItems))
 console.log(galleryItems);
