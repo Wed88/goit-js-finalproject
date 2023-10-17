f120bf283051352093966eaf3cbf185856360b36
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-10-18 00:19:50 +0300

    додає виконання 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItemsMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+    `).join('')
+};
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+ });
+
+
 console.log(galleryItems);
